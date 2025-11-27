476a3d984b28cd544112bd48accab8c71f8f8746
lcp19840214 <lcp19840214@hotmail.com>
2025-11-27 22:24:43 +0800

    code

---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# git config user.name "lcp19840214" ; git config user.email "lcp19840214@hotmail.com" ;  git config commit.gpgsign true ; git config user.signingkey 66B451B92928202F38C446145F2C469F2D37EB26
+
+git add .
+git commit -m "code"
+git push
---
+++ src/main/java/com/lcp/spb/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.lcp.spb.config;
+
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  CorsWebFilter corsWebFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOriginPatterns(Collections.singletonList("*"));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return new CorsWebFilter(source);
+  }
+}
